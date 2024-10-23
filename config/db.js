9107eb4a3c62ae38b4d91265b5dadc85d6dbd3a7
aakashdixit22 <aakashdixit227@gmail.com>
2024-10-23 14:04:35 +0530

    Update db connection

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test-portal", {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
